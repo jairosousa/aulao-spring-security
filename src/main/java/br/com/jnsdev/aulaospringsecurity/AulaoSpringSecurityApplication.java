cd08a24e7ed0dbcc555944dcf5940a1681fa3929
Jairo Nascimento <jaironsousa@gmail.com>
2023-12-19 06:14:21 -0300

    Criação resources sem segurança   - Alteração versão Spring-Boot para 3.0.0

--- src/main/java/br/com/jnsdev/aulaospringsecurity/AulaoSpringSecurityApplication.java
+++ src/main/java/br/com/jnsdev/aulaospringsecurity/AulaoSpringSecurityApplication.java
@@ -2,12 +2,29 @@ package br.com.jnsdev.aulaospringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AulaoSpringSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AulaoSpringSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AulaoSpringSecurityApplication.class, args);
+    }
 
+    @RestController
+    class HttpController {
+        @GetMapping("publica")
+        public String publicRoute() {
+            return "<h1>Public route, feel to look around 🔓</h1>";
+
+        }
+
+        @GetMapping("privada")
+        public String privateRoute() {
+            return "<h1>Private route, only authorized personal 🔏</h1>";
+
+        }
+
+    }
 }
